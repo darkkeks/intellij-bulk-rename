65f4d011eea7ad6899fd43cd2279e668a49372f0
darkkeks <darkkeks@rambler.ru>
2021-02-28 00:29:35 +0300

    Working prototype

--- README.md
+++ README.md
@@ -1,24 +1,19 @@
 # intellij-bulk-rename
 
 ![Build](https://github.com/DarkKeks/intellij-bulk-rename/workflows/Build/badge.svg)
-[![Version](https://img.shields.io/jetbrains/plugin/v/PLUGIN_ID.svg)](https://plugins.jetbrains.com/plugin/PLUGIN_ID)
-[![Downloads](https://img.shields.io/jetbrains/plugin/d/PLUGIN_ID.svg)](https://plugins.jetbrains.com/plugin/PLUGIN_ID)
-
-## Template ToDo list
-- [x] Create a new [IntelliJ Platform Plugin Template][template] project.
-- [ ] Verify the [pluginGroup](/gradle.properties), [plugin ID](/src/main/resources/META-INF/plugin.xml) and [sources package](/src/main/kotlin).
-- [ ] Review the [Legal Agreements](https://plugins.jetbrains.com/docs/marketplace/legal-agreements.html).
-- [ ] [Publish a plugin manually](https://plugins.jetbrains.com/docs/intellij/publishing-plugin.html?from=IJPluginTemplate) for the first time.
-- [ ] Set the Plugin ID in the above README badges.
-- [ ] Set the [Deployment Token](https://plugins.jetbrains.com/docs/marketplace/plugin-upload.html).
-- [ ] Click the <kbd>Watch</kbd> button on the top of the [IntelliJ Platform Plugin Template][template] to be notified about releases containing new features and fixes.
 
 <!-- Plugin description -->
-This Fancy IntelliJ Platform Plugin is going to be your implementation of the brilliant ideas that you have.
+Мне нужен был способ безопасно переименовать несколько сотен методов.
+Попытки сделать это через `grep + sed` потерпели неудачу, так как пришлось бы руками фильтровать использования методов с совпадающими названиями.
 
-This specific section is a source for the [plugin.xml](/src/main/resources/META-INF/plugin.xml) file which will be extracted by the [Gradle](/build.gradle.kts) during the build process.
+Плагином добавляется action `Bulk Rename`, который принимает на вход текст в формате
+```
+fully.qualified.class.Name,fromMethodName,toMethodName
+fully.qualified.class.Name,fromMethodName,toMethodName
+fully.qualified.class.Name,fromMethodName,toMethodName
+```
 
-To keep everything working, do not remove `<!-- ... -->` sections. 
+Для каждой строки дергается дефолтный rename без поиска в коментариях и "не java" файлах, чтобы не показывать диалог выбора что переименовывать, а что нет.
 <!-- Plugin description end -->
 
 ## Installation
---
+++ src/main/kotlin/com/github/darkkeks/intellijbulkrename/BulkRenameAction.kt
@@ -0,0 +1,85 @@
+package com.github.darkkeks.intellijbulkrename
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.ui.Messages
+import com.intellij.psi.JavaPsiFacade
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.refactoring.RefactoringFactory
+import java.awt.BorderLayout
+import java.awt.Dimension
+import javax.swing.JComponent
+import javax.swing.JPanel
+import javax.swing.JTextArea
+
+
+class BulkRenameAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.getRequiredData(CommonDataKeys.PROJECT)
+        val dialog = BulkRenameDialog(project)
+
+        if (dialog.showAndGet()) {
+            val text = dialog.field.text
+            for (line in text.lines()) {
+                val (className, fromMethod, toMethod) = line.split(",")
+                if (!renameMethod(project, className, fromMethod, toMethod)) {
+                    return
+                }
+            }
+            Messages.showMessageDialog(project, "Renamed successfully", "Bulk Rename", null)
+        }
+    }
+
+    private fun renameMethod(project: Project, className: String, fromMethod: String, toMethod: String): Boolean {
+        val clazz =
+            JavaPsiFacade.getInstance(project).findClass(className, GlobalSearchScope.allScope(project))
+        if (clazz == null) {
+            Messages.showMessageDialog(project, "Cant find class $className", "Bulk Rename Failed", null)
+            return false
+        }
+
+        val method = clazz.allMethods
+            .find { it.name == fromMethod }
+
+        if (method == null) {
+//            Messages.showMessageDialog(
+//                project,
+//                "Cant find method $fromMethod in class ${clazz.name}",
+//                "Bulk Rename",
+//                null
+//            )
+            return true
+        }
+
+        val superMethods = method.findDeepestSuperMethods()
+        val toRename = superMethods.firstOrNull() ?: method
+
+        val refactoring = RefactoringFactory.getInstance(project)
+            .createRename(toRename, toMethod)
+        refactoring.isSearchInComments = false
+        refactoring.isSearchInNonJavaFiles = false
+        refactoring.setInteractive(null)
+        refactoring.run()
+
+        return true
+    }
+
+    class BulkRenameDialog(project: Project) : DialogWrapper(project, false) {
+        val field = JTextArea("Input here")
+
+        override fun createCenterPanel(): JComponent {
+            val dialogPanel = JPanel(BorderLayout())
+            field.preferredSize = Dimension(300, 200)
+            dialogPanel.add(field, BorderLayout.CENTER)
+            return dialogPanel
+        }
+
+        init {
+            init()
+            title = "Bulk Rename"
+        }
+    }
+}
