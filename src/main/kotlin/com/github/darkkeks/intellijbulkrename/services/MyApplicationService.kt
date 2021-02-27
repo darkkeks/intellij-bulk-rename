fb9c12cb3b6c985a52fceffee72b2a3217e0f8b1
GitHub Action <action@github.com>
2021-02-27 17:21:56 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/darkkeks/intellijbulkrename/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.darkkeks.intellijbulkrename.services
+
+import com.github.darkkeks.intellijbulkrename.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/darkkeks/intellijbulkrename/services/MyProjectService.kt
+++ src/main/kotlin/com/github/darkkeks/intellijbulkrename/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.darkkeks.intellijbulkrename.services
 
+import com.github.darkkeks.intellijbulkrename.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
